0d1dbe199402385f636cb7f55df68db38622ffa3
Shin'ichiro SUZUKI <shin@sforzando.co.jp>
2018-01-06 23:12:28 +0900

    add koa-favicon

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Koa = require('koa');
-const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
+const favicon = require('koa-favicon');
+const Router = require('koa-router');
 
 const app = new Koa();
 const router = new Router();
@@ -12,6 +13,7 @@ router.get('/', async (ctx, next) => {
 app.use(router.routes());
 app.use(router.allowedMethods());
 app.use(bodyParser());
+app.use(favicon(__dirname + '/favicon.ico'));
 app.listen(process.env.PORT || 3000);
 
 const puppeteer = require('puppeteer');
