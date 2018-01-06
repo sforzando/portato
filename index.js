9dc8265d447e4c73c4b77d2b2d63a085ff68e4cd
Shin'ichiro SUZUKI <shin@sforzando.co.jp>
2018-01-06 21:57:23 +0900

    add Koa

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const puppeteer = require('puppeteer');
+const Koa = require('koa');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://news.ycombinator.com', { waitUntil: 'networkidle2' });
-  const newsTitles = await page.evaluate(() => {
-    const elenemts = document.querySelectorAll('.itemlist .title > a');
-    return [].map.call(elenemts, el => el.innerText);
-  });
-  console.log(newsTitles.slice(0, 5));
-  await browser.close();
-})();
+const app = new Koa();
+const router = new Router();
+
+router.get('/', async (ctx, next) => {
+  ctx.body = 'Hello World';
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+app.use(bodyParser());
+app.listen(process.env.PORT || 3000);
