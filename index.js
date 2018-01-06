3d559ff0f3dceb03270d7ff324c6999fad2e5794
Shin'ichiro SUZUKI <shin@sforzando.co.jp>
2018-01-07 03:44:25 +0900

    implement puppeteer logics

--- index.js
+++ index.js
@@ -26,13 +26,26 @@ const LAUNCH_OPTION = process.env.DYNO ? {
 const crawler = async () => {
   const browser = await puppeteer.launch(LAUNCH_OPTION);
   const page = await browser.newPage();
-  await page.goto('https://news.ycombinator.com', {
+  await page.goto('https://my.zenlogic.jp/login/', {
     waitUntil: 'networkidle2'
   });
-  const newsTitles = await page.evaluate(() => {
-    const elenemts = document.querySelectorAll('.itemlist .title > a');
-    return [].map.call(elenemts, el => el.innerText);
+
+  await page.type('#account_username', 'xxxxxxxx');
+  await page.type('#account_password', 'xxxxxxxx');
+  await page.click('form input[type=submit]');
+  await page.waitForNavigation();
+
+  await page.goto('https://my.zenlogic.jp/configurations/27822/mail/mailbox_sizes', {
+    waitUntil: 'networkidle2'
+  });
+  await page.select('select.mr5', 'j-monkey.jp');
+  await page.waitFor(1000);
+
+  await page.screenshot({path: 'screenshot.png', fullPage: true});
+  const output = await page.evaluate(() => {
+    const elements = document.querySelectorAll('tr');
+    return [].map.call(elements, el => el.innerText.split('\t'));
   });
   await browser.close();
-  return newsTitles;
+  return output;
 }
