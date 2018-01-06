af12bc144dccc9eced77dcf81ec5176a4fc66c84
Shin'ichiro SUZUKI <shin@sforzando.co.jp>
2018-01-06 21:59:36 +0900

    implement crawler logics

--- index.js
+++ index.js
@@ -6,10 +6,31 @@ const app = new Koa();
 const router = new Router();
 
 router.get('/', async (ctx, next) => {
-  ctx.body = 'Hello World';
+  ctx.body = await crawler();
 });
 
 app.use(router.routes());
 app.use(router.allowedMethods());
 app.use(bodyParser());
 app.listen(process.env.PORT || 3000);
+
+const puppeteer = require('puppeteer');
+const LAUNCH_OPTION = process.env.DYNO ? {
+  args: ['--no-sandbox', '--disable-setuid-sandbox']
+} : {
+  headless: false
+};
+
+const crawler = async () => {
+  const browser = await puppeteer.launch(LAUNCH_OPTION); // Launch Optionの追加
+  const page = await browser.newPage();
+  await page.goto('https://news.ycombinator.com', {
+    waitUntil: 'networkidle2'
+  });
+  const newsTitles = await page.evaluate(() => {
+    const elenemts = document.querySelectorAll('.itemlist .title > a');
+    return [].map.call(elenemts, el => el.innerText);
+  });
+  await browser.close();
+  return newsTitles;
+}
